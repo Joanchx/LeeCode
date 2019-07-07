8396b75bee9b9a016d8a1d49a7770fdc9f9917d1
chenxq <603600864@qq.com>
2019-07-07 23:44:07 +0800

    problems 0557

--- code/0557_reverse-words-in-a-string-iii/0557_reverse-words-in-a-string-iii.js
+++ code/0557_reverse-words-in-a-string-iii/0557_reverse-words-in-a-string-iii.js
@@ -1,5 +1,5 @@
 /**
- * @date 2019-07-08
+ * @date 2019-07-07
  * https://leetcode.com/problems/reverse-words-in-a-string-iii/
  * --------------------------------------------------------------------------------------------------------
  * Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
@@ -9,7 +9,8 @@
  * Output: "s'teL ekat edoCteeL tsetnoc"
  * Note: In the string, each word is separated by single space and there will not be any extra space in the string.
  * --------------------------------------------------------------------------------------------------------
- * 解题思路：遍历数组，寻找目标索引。
+ * 解题思路： 1、字符串按空格进行分隔；
+ *           2、对每个元素进行反转；
  *  */
 
 /**
@@ -17,9 +18,7 @@
 * @return {string}
 */
 var reverseWords = function (s) {
-    const array = s.split(' ')
-    const r = array.map(item => item.split('').reverse().join(''))
-    return r.join(' ')
+    return s.split(' ').map(item => item.split('').reverse().join('')).join(' ')
 };
 
 module.exports = reverseWords;
