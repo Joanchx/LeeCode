cfdb788d204677dc072f390b29d38121533cf34c
chenxq <603600864@qq.com>
2021-05-03 17:38:21 +0800

    problems 0001

--- code/0001_two-sum/0001_two-sum.js
+++ code/0001_two-sum/0001_two-sum.js
@@ -16,15 +16,15 @@
  *  @param {Array} nums
  *  @param {Number} target
  * */
-const twoSum = function (nums, target) {
-	for (let i = 0; i < nums.length; i++) {
-		for (let j = i + 1; j < nums.length; j++) {
-			if (nums[i] + nums[j] == target) {
-				return [i, j];
-			}
-		}
-	}
-};
+// const twoSum = function (nums, target) {
+// 	for (let i = 0; i < nums.length; i++) {
+// 		for (let j = i + 1; j < nums.length; j++) {
+// 			if (nums[i] + nums[j] == target) {
+// 				return [i, j];
+// 			}
+// 		}
+// 	}
+// };
 
 
 /**
